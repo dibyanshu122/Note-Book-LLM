41b0720f543d30bb16703a41303f0124fd7e9966
Dibyanshu <126516699+dibyanshu122@users.noreply.github.com>
2026-05-08 22:03:21 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-
-  turbopack: {
-    root: __dirname,
+  experimental: {
+    // Ye line dono values ko sync kar degi
+    outputFileTracingRoot: path.join(__dirname, "../../"), 
   },
-
+  // ... aapki baaki config
 };
 
 export default nextConfig;
